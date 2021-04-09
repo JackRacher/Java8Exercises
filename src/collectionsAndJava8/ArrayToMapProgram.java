4a4e2ce593c06bba56a9977b85e49bafcfca9936
pb21071 <pb21071@OJAS00219.corp.ojas-it.com>
2021-04-09 13:56:55 +0530

    Added

--- src/collectionsAndJava8/ArrayToMapProgram.java
+++ src/collectionsAndJava8/ArrayToMapProgram.java
@@ -1,5 +1,26 @@
 package collectionsAndJava8;
 
+import java.util.HashMap;
+import java.util.Map;
+
+class ArrayToMap{
+	public static Map<Integer, String> arrayToMap(String[] array){
+		if(array == null)
+			return null;
+		
+		Map<Integer, String> stringMap = new HashMap<Integer, String>();
+		for (int i = 0; i < array.length; i++) {
+			
+			stringMap.put((i + 1), array[i]);
+		}
+		return stringMap;
+	}
+}
 public class ArrayToMapProgram {
 
+	public static void main(String[] args) {
+		String[] array = {"Ram", "Sam", "Jam", "Tom"};
+		
+		System.out.println(ArrayToMap.arrayToMap(array));
+	}
 }
