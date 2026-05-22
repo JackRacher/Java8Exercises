2904e36ce8f073340cfe75aff190767cd30c1fc8
PRUTHVI <PRUTHVI@LAPTOP-9C39FJ97>
2026-05-22 17:56:00 +0530

    modified

--- src/collectionsAndJava8/CountOccurence.java
+++ src/collectionsAndJava8/CountOccurence.java
@@ -1,7 +1,6 @@
 package collectionsAndJava8;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,8 +8,8 @@ import java.util.stream.Collectors;
 public class CountOccurence {
 
 	public static void main(String[] args) {
-		String name = "java";
-		String[] chars = name.split("");
+		//String name = "java";
+		//String[] chars = name.split("");
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(10);
 		list.add(40);
